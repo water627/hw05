38a7011f9f89411d0b5dc6c055f72abd87d5fe96
water627 <191664051@qq.com>
2026-05-08 19:52:44 +0800

    Implement LeNet5 architecture in lenet5.py

---
+++ task2_lenet5/lenet5.py
@@ -0,0 +1,23 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class LeNet5(nn.Module):
+    def __init__(self):
+        super(LeNet5, self).__init__()
+        self.conv1 = nn.Conv2d(1, 6, kernel_size=5, padding=2)
+        self.pool1 = nn.AvgPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, kernel_size=5)
+        self.pool2 = nn.AvgPool2d(2, 2)
+        self.conv3 = nn.Conv2d(16, 120, kernel_size=5)
+        self.fc1 = nn.Linear(120, 84)
+        self.fc2 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool1(torch.tanh(self.conv1(x)))
+        x = self.pool2(torch.tanh(self.conv2(x)))
+        x = torch.tanh(self.conv3(x))
+        x = x.view(-, 120)
+        x = torch.tanh(self.fc1(x))
+        x = self.fc2(x)
+        return x
